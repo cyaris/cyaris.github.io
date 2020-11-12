bf12c83a8b1ae09b9d8a96fee8510138bf177e06
cyaris <charlieyaris@gmail.com>
2020-11-12 14:02:55 -0600

    More improvements to preprocessing leading to json file upload.

--- _includes/the_networks_of_war/the_networks_of_war_python_functions.py
+++ _includes/the_networks_of_war/the_networks_of_war_python_functions.py
@@ -3,23 +3,23 @@ import numpy as np
 from copy import deepcopy
 from traceback import format_exc
 
-def participant_start_and_end_dates(participant_dataframe):
+def participant_start_and_end_dates(dataframe):
 
     ## defining null values (missing data)
-    participant_dataframe.loc[participant_dataframe['start_day']==-9, 'start_day'] = None
-    participant_dataframe.loc[participant_dataframe['start_month']==-9, 'start_month'] = None
-    participant_dataframe.loc[participant_dataframe['start_year']==-9, 'start_year'] = None
-    participant_dataframe.loc[participant_dataframe['end_day']==-9, 'end_day'] = None
-    participant_dataframe.loc[participant_dataframe['end_month']==-9, 'end_month'] = None
-    participant_dataframe.loc[participant_dataframe['end_year']==-9, 'end_year'] = None
+    dataframe.loc[dataframe['start_day']==-9, 'start_day'] = None
+    dataframe.loc[dataframe['start_month']==-9, 'start_month'] = None
+    dataframe.loc[dataframe['start_year']==-9, 'start_year'] = None
+    dataframe.loc[dataframe['end_day']==-9, 'end_day'] = None
+    dataframe.loc[dataframe['end_month']==-9, 'end_month'] = None
+    dataframe.loc[dataframe['end_year']==-9, 'end_year'] = None
 
     ## creatign a field for ongoing wars
-    participant_dataframe.loc[participant_dataframe['end_day']==-7, 'ongoing_participation'] = 1
-    participant_dataframe.loc[participant_dataframe['ongoing_participation'].isnull(), 'ongoing_participation'] = 0
+    dataframe.loc[dataframe['end_day']==-7, 'ongoing_participation'] = 1
+    dataframe.loc[dataframe['ongoing_participation'].isnull(), 'ongoing_participation'] = 0
     ## adjusting for ongoing with wars with current date end date
-    participant_dataframe.loc[participant_dataframe['end_day']==-7, 'end_day'] = None
-    participant_dataframe.loc[participant_dataframe['end_month']==-7, 'end_month'] = None
-    participant_dataframe.loc[participant_dataframe['end_year']==-7, 'end_year'] = None
+    dataframe.loc[dataframe['end_day']==-7, 'end_day'] = None
+    dataframe.loc[dataframe['end_month']==-7, 'end_month'] = None
+    dataframe.loc[dataframe['end_year']==-7, 'end_year'] = None
 
     ## calculating null for all without valid start/end dates.
     ## those with invalid data will have null values.
@@ -27,63 +27,63 @@ def participant_start_and_end_dates(participant_dataframe):
     dates_found = 0
     dates_not_found = 0
 
-    for i, date in enumerate(participant_dataframe['war_num']):
+    for i, date in enumerate(dataframe['war_num']):
 
         ## unsure why this occurs (.0 after integer)
-        participant_dataframe.loc[i, 'start_day'] = str(participant_dataframe.loc[i, 'start_day']).split('.')[0]
-        participant_dataframe.loc[i, 'start_month'] = str(participant_dataframe.loc[i, 'start_month']).split('.')[0]
-        participant_dataframe.loc[i, 'start_year'] = str(participant_dataframe.loc[i, 'start_year']).split('.')[0]
-        participant_dataframe.loc[i, 'end_day'] = str(participant_dataframe.loc[i, 'end_day']).split('.')[0]
-        participant_dataframe.loc[i, 'end_month'] = str(participant_dataframe.loc[i, 'end_month']).split('.')[0]
-        participant_dataframe.loc[i, 'end_year'] = str(participant_dataframe.loc[i, 'end_year']).split('.')[0]
+        dataframe.loc[i, 'start_day'] = str(dataframe.loc[i, 'start_day']).split('.')[0]
+        dataframe.loc[i, 'start_month'] = str(dataframe.loc[i, 'start_month']).split('.')[0]
+        dataframe.loc[i, 'start_year'] = str(dataframe.loc[i, 'start_year']).split('.')[0]
+        dataframe.loc[i, 'end_day'] = str(dataframe.loc[i, 'end_day']).split('.')[0]
+        dataframe.loc[i, 'end_month'] = str(dataframe.loc[i, 'end_month']).split('.')[0]
+        dataframe.loc[i, 'end_year'] = str(dataframe.loc[i, 'end_year']).split('.')[0]
 
         try:
-            participant_dataframe.loc[i, 'start_date'] = pd.to_datetime(participant_dataframe.loc[i, 'start_year'] + "-" + participant_dataframe.loc[i, 'start_month'] + "-" + participant_dataframe.loc[i, 'start_day'])
+            dataframe.loc[i, 'start_date'] = pd.to_datetime(dataframe.loc[i, 'start_year'] + "-" + dataframe.loc[i, 'start_month'] + "-" + dataframe.loc[i, 'start_day'])
             valid_start_date = 1
         except:
             valid_start_date = 0
-            participant_dataframe.loc[i, 'start_date'] = None
+            dataframe.loc[i, 'start_date'] = None
         try:
-            participant_dataframe.loc[i, 'end_date'] = pd.to_datetime(participant_dataframe.loc[i, 'end_year'] + "-" + participant_dataframe.loc[i, 'end_month'] + "-" + participant_dataframe.loc[i, 'end_day'])
+            dataframe.loc[i, 'end_date'] = pd.to_datetime(dataframe.loc[i, 'end_year'] + "-" + dataframe.loc[i, 'end_month'] + "-" + dataframe.loc[i, 'end_day'])
             valid_end_date = 1
         except:
             valid_end_date = 0
-            participant_dataframe.loc[i, 'end_date'] = None
+            dataframe.loc[i, 'end_date'] = None
 
         if valid_start_date==1 and valid_end_date==1:
-            participant_dataframe.loc[i, 'days_at_war'] = participant_dataframe.loc[i, 'end_date'] - participant_dataframe.loc[i, 'start_date']
-            participant_dataframe.loc[i, 'days_at_war'] = int(str(participant_dataframe.loc[i, 'days_at_war']).split(' ')[0])
+            dataframe.loc[i, 'days_at_war'] = dataframe.loc[i, 'end_date'] - dataframe.loc[i, 'start_date']
+            dataframe.loc[i, 'days_at_war'] = int(str(dataframe.loc[i, 'days_at_war']).split(' ')[0])
         else:
-            participant_dataframe.loc[i, 'days_at_war'] = None
+            dataframe.loc[i, 'days_at_war'] = None
 
         if valid_start_date + valid_end_date==2:
             dates_found += 1
         else:
             dates_not_found+=1
 
-    print("total rows with both dates found {}".format(dates_found))
-    print("total rows with at least one date not found {}\n".format(dates_not_found))
+    print("total rows with both dates found: {}".format(dates_found))
+    print("total rows with at least one date not found: {}\n".format(dates_not_found))
 
-    return participant_dataframe
+    return dataframe
 
 
-def remaining_participant_null_values(participant_dataframe):
+def remaining_participant_null_values(dataframe):
 
     ## defining null values (missing data)
     ## -8 is not applicable.
-    participant_dataframe.loc[participant_dataframe['battle_deaths']==-8, 'battle_deaths'] = None
-    participant_dataframe.loc[participant_dataframe['total_deaths_both_sides']==-8, 'total_deaths_both_sides'] = None
-    participant_dataframe.loc[participant_dataframe['peak_forces_available']==-8, 'peak_forces_available'] = None
-    participant_dataframe.loc[participant_dataframe['peak_battle_forces']==-8, 'peak_battle_forces'] = None
-    participant_dataframe.loc[participant_dataframe['lagging_war']==-8, 'lagging_war'] = None
-    participant_dataframe.loc[participant_dataframe['leading_war']==-8, 'leading_war'] = None
+    dataframe.loc[dataframe['battle_deaths']==-8, 'battle_deaths'] = None
+    dataframe.loc[dataframe['total_deaths_both_sides']==-8, 'total_deaths_both_sides'] = None
+    dataframe.loc[dataframe['peak_forces_available']==-8, 'peak_forces_available'] = None
+    dataframe.loc[dataframe['peak_battle_forces']==-8, 'peak_battle_forces'] = None
+    dataframe.loc[dataframe['lagging_war']==-8, 'lagging_war'] = None
+    dataframe.loc[dataframe['leading_war']==-8, 'leading_war'] = None
     ## -9 is unknown.
-    participant_dataframe.loc[participant_dataframe['battle_deaths']==-9, 'battle_deaths'] = None
-    participant_dataframe.loc[participant_dataframe['total_deaths_both_sides']==-9, 'total_deaths_both_sides'] = None
-    participant_dataframe.loc[participant_dataframe['peak_forces_available']==-9, 'peak_forces_available'] = None
-    participant_dataframe.loc[participant_dataframe['peak_battle_forces']==-9, 'peak_battle_forces'] = None
+    dataframe.loc[dataframe['battle_deaths']==-9, 'battle_deaths'] = None
+    dataframe.loc[dataframe['total_deaths_both_sides']==-9, 'total_deaths_both_sides'] = None
+    dataframe.loc[dataframe['peak_forces_available']==-9, 'peak_forces_available'] = None
+    dataframe.loc[dataframe['peak_battle_forces']==-9, 'peak_battle_forces'] = None
 
-    return participant_dataframe
+    return dataframe
 
 
 def union_opposite_columns(dataframe, switched_columns_list):
@@ -116,5 +116,46 @@ def drop_participant_b_columns(dataframe, switched_columns_list):
     for column in switched_columns_list:
         if column[-2:]=='_a':
             dataframe.rename({column: column[:-2]}, axis=1, inplace=True)
+
+    return dataframe
+
+
+def column_fills_and_converions(dataframe, conversion_dic):
+
+    column_list = list(dataframe.columns)
+    x_y_columns = []
+
+    for i, column in enumerate(column_list):
+        if column[-2:]=='_x':
+            x_y_columns.append(column)
+        elif column[-2:]=='_y':
+            x_y_columns.append(column)
+
+    null_values = 0
+    unknown_values = 0
+    ## filling in nulls with zeros
+    ## these are ones that most likely mean zero if null (not due to missing data)
+    for column in x_y_columns:
+        null_values = null_values + len(dataframe[dataframe[column].isnull()])
+        ## giving this zeros because they weren't in the dataset at all.
+        ## no result following join.
+        dataframe.loc[dataframe[column].isnull(), column] = 0
+        ## -9 is unknown value in the dataset
+        ## giving these null values
+        unknown_values = unknown_values + len(dataframe[dataframe[column]==-9])
+        dataframe.loc[dataframe[column]==-9, column] = None
+        if conversion_dic==None:
+            pass
+        elif column[:-2] in list(conversion_dic.keys()):
+            # converting these to their proper units according to documentation
+            dataframe[column] = ([s * conversion_dic[column[:-2]] for s in dataframe[column]])
+
+    print('\ntotal columns adjusted: {}'.format(len(x_y_columns)))
+    if conversion_dic==None:
+        print('total columns adjusted for conversion: 0')
+    else:
+        print('total columns adjusted for conversion: {}'.format(len(list(conversion_dic.keys())) * 2))
+    print('null values notated: {}'.format(null_values))
+    print('unknown values notated: {}'.format(unknown_values))
 
     return dataframe

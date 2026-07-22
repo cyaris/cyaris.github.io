0da0dbbe92040718a1261b2ecd98e2ea8e4c9127
Charlie Yaris <charlieyaris@charlies-mbp.mynetworksettings.com>
2026-07-21 23:06:26 -0500

    Fix typed post pagination

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,15 +1,9 @@
 module.exports = {
   arrowParens: "avoid",
+  objectWrap: "collapse",
   printWidth: 120,
   semi: false,
-  trailingComma: "es5",
+  trailingComma: "none",
   plugins: [require.resolve("@shopify/prettier-plugin-liquid")],
-  overrides: [
-    {
-      files: ["*.html", "*.liquid"],
-      options: {
-        parser: "liquid-html",
-      },
-    },
-  ],
+  overrides: [{ files: ["*.html", "*.liquid"], options: { parser: "liquid-html" } }]
 }
